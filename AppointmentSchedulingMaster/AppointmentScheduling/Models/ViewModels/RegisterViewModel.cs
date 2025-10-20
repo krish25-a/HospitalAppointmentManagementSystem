0b0dae27823a277d2c085bea74cd165a77e83fe2
krish25-a <krishpatel60497@gmail.com>
2025-10-20 18:44:25 +0530

    Update RegisterViewModel.cs

--- AppointmentSchedulingMaster/AppointmentScheduling/Models/ViewModels/RegisterViewModel.cs
+++ AppointmentSchedulingMaster/AppointmentScheduling/Models/ViewModels/RegisterViewModel.cs
@@ -1,1 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentScheduling.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage ="The password must be atleast 6 characters long.")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage ="The passwords don't match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
+    }
+}
 
